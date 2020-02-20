cf89b8866b17c6200258be1fb4c17db315ba062d
Max Wibert <xwibert@gmail.com>
2020-02-20 15:16:23 -0600

    created class methods to parse individual tle sets and entire textfiles in orbital.py testing starting soon

--- orbital.py
+++ orbital.py
@@ -56,7 +56,6 @@ class TLE:
         self.BSTAR = parse_float(self.BSTAR)
 
         self.element_num = int(self.element_num)
-        self.satellite_num = int(self.satellite_num)
 
         self.inclination = float(self.inclination)
         self.RAAD = float(self.RAAD)
@@ -68,5 +67,48 @@ class TLE:
 
         return self
 
+    #takes: path to TLE.txt file
+    #returns: list of TLE instances with parameters matching specified by text file.
+    @classmethod
+    def parse_file(cls,path):
+        with open(path, "r") as io:
+            lines = io.readlines()
+            io.close()
+        line_bunches = [lines[i:i+2] for i in range(0, len(lines), 3)]
+        return map(lambda bunch: cls.parse_tle(bunch),line_bunches)
 
-    def generate_tles(path):
+
+    
+
+    #takes: three lines: the name followed by the tle lines in string form
+    #returns: TLE instance with params determined by tle lines
+    @classmethod
+    def parse_tle(cls, line0, line1, line2):
+        params = {
+            #line 0 is just the name of the satellite
+            name: line0,
+            #line 1 params
+            sat_num: line1[2:6],
+            classification: line1[7],
+            id_year: line1[9:10],
+            id_launch: line1[11:12],
+            id_piece: line1[14:16],
+            epoch_year: line1[18:19],
+            epoch: line1[20:31],
+            mean_motion_p: line1[33:42],
+            mean_motion_pp: line1[44:51],
+            BSTAR: line1[53:60],
+            element_num: line1[64:67],
+            #line 2 params
+            inclination: line2[8:15],
+            RAAD: line2[17:24],
+            eccentricity: line2[26:32],
+            arg_perigee: line2[34:41],
+            mean_anomaly: line2[43:50],
+            mean_motion: line2[52:62],
+            rev_num: line2[63:67]
+        }
+
+        return TLE(params)
+
+    
